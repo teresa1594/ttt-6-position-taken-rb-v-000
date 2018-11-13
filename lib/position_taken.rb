1108cdd88b5179a42ff93de9ca37a04d38a18a46
Teresa De Jesus <teresa1594@gmail.com>
2018-11-13 01:03:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
-def valid_move?(board,position)
-  position.to_i.between?(1,9) and not position_taken?(board, position.to_i - 1)
+def valid_move?(board,index)
+  index.to_i.between?(1,9) and not position_taken?(board, index.to_i - 1)
 end
 
-def position_taken?(board,position)
-  return false if [" ", "", nil].include?(board[position])
-  return true if ["X", "O"].include?(board[position])
-  raise "#{board[position]} is not a valid move"
+def position_taken?(board,index)
+  return false if [" ", "", nil].include?(board[index])
+  return true if ["X", "O"].include?(board[index])
+  raise "#{board[index]} is not a valid move"
 end
